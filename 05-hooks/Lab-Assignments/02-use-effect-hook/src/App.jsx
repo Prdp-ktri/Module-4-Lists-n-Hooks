a47f3cc296c70d518759ad65fdeeec66e2ec5082
Pradeep Khatri <pradeepkhatri44@live.com>
2025-09-29 14:12:19 +0530

    commit 2nd Lab answer
    commit 2nd Lab answer

--- 05-hooks/Lab-Assignments/02-use-effect-hook/src/App.jsx
+++ 05-hooks/Lab-Assignments/02-use-effect-hook/src/App.jsx
@@ -3,12 +3,24 @@ import "./App.css";
 
 function App() {
   const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos")
-      .then((res) => res.json())
+      .then((res) => {
+        if(!res.ok){
+          throw new Error("Network was not ok")
+        }
+        return res.json();
+      })
       .then((data) => {
         setPhotos(data);
+        setLoading(false);
+      })
+      .catch((err)=>{
+        setError(err.message);
+        setLoading(false);
       });
   }, []);
 
