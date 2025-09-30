79c69f3c2ba95ece76f5dc39128cf47361eaac47
Pradeep Khatri <pradeepkhatri44@live.com>
2025-09-30 13:30:16 +0530

    commit 03-redux assignment
    commit 03-redux assignment

--- 05-hooks/Lab-Assignments/03-redux-lab-assignment/src/App.js
+++ 05-hooks/Lab-Assignments/03-redux-lab-assignment/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment, reset } from "./features/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      
+      <h2>Redux Counter App</h2>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>+ Increment</button>
+      <button onClick={() => dispatch(decrement())}>- Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
 }
